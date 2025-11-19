3aaed51a137209e1456e892ff0e54840b6703508
Tiger5286 <toratiger5286@gmail.com>
2025-11-19 18:46:36 +0900

    背景のためにいろいろやったけどあんまりよくわからん

---
+++ reploid_bg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="reploid_bg" tilewidth="16" tileheight="16" tilecount="325" columns="25">
+ <image source="mapchip_bg.jpg" trans="ffffff" width="400" height="208"/>
+</tileset>
