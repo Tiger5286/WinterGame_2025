368b41ba41c40ae8ed7e4da1b164d0f04098b726
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2025-12-01 02:30:54 +0900

    EnemyManagerクラス実装

--- WinterGame_2025/Source/Systems/EnemyManager.cpp
+++ WinterGame_2025/Source/Systems/EnemyManager.cpp
@@ -3,12 +3,16 @@
 #include "Dxlib.h"
 #include <cassert>
 
+#include "../Systems/Camera.h"
+
 #include "../GameObjects/Enemies/WalkEnemy.h"
 #include "../GameObjects/Enemies/FlyEnemy.h"
 #include "../GameObjects/Enemies/JumpEnemy.h"
 
-EnemyManager::EnemyManager(std::shared_ptr<Player> pPlayer):
-	_pPlayer(pPlayer)
+EnemyManager::EnemyManager(std::shared_ptr<Player> pPlayer, std::shared_ptr<Map> pMap, std::shared_ptr<Camera> pCamera):
+	_pPlayer(pPlayer),
+	_pMap(pMap),
+	_pCamera(pCamera)
 {
 	_walkEnemyH = LoadGraph("data/Enemys/WalkEnemy.png");
 	assert(_walkEnemyH != -1);
@@ -16,7 +20,6 @@ EnemyManager::EnemyManager(std::shared_ptr<Player> pPlayer):
 	assert(_flyEnemyH != -1);
 	_jumpEnemyH = LoadGraph("data/Enemys/JumpEnemy.png");
 	assert(_jumpEnemyH != -1);
-
 }
 
 EnemyManager::~EnemyManager()
@@ -25,10 +28,42 @@ EnemyManager::~EnemyManager()
 
 void EnemyManager::Update()
 {
+	for (auto& enemy : _pEnemies)
+	{
+		if (enemy != nullptr)
+		{
+			float toCameraDisX = enemy->GetPos().x - _pCamera->GetPos().x;	// “G‚ÆƒJƒƒ‰‚Ì‹——£
+			// ƒJƒƒ‰‚Ì‰æ–Ê“à‚É‚¢‚é“G‚¾‚¯XV‚·‚é
+			if (abs(toCameraDisX) < GlobalConstants::kScreenWidth / 2 + 100)
+			{
+				enemy->Update(*_pMap);
+			}
+			// ‘Ì—Í‚ª0ˆÈ‰º‚É‚È‚Á‚½‚çÁ‚·
+			if (enemy->GetHp() <= 0)
+			{
+				enemy->Delete();
+				// €‚ñ‚¾“G‚ğvector‚©‚çíœ‚·‚é(‚ ‚ñ‚ÜˆÓ–¡‚í‚©‚ç‚ñ)
+				_pEnemies.erase(
+					std::remove_if(_pEnemies.begin(), _pEnemies.end(),
+						[](const std::shared_ptr<Enemy>& enemy) {
+							return !enemy->GetIsAlive();
+						}),
+					_pEnemies.end()
+				);
+			}
+		}
+	}
 }
 
 void EnemyManager::Draw()
 {
+	for (auto& enemy : _pEnemies)
+	{
+		if (enemy != nullptr)
+		{
+			enemy->Draw(_pCamera->GetDrawOffset());
+		}
+	}
 }
 
 void EnemyManager::Spawn(const std::vector<uint16_t>& objectData, Size size)
@@ -39,10 +74,26 @@ void EnemyManager::Spawn(const std::vector<uint16_t>& objectData, Size size)
 		{
 			auto index = w + h * size.w;
 			auto data = static_cast<ObjectData>(objectData[index]);
+			Vector2 pos = Vector2(static_cast<float>(w), static_cast<float>(h));
 			switch (data)
 			{
 			case ObjectData::WalkEnemyIdle:
-				_enemies.push_back(std::make_shared<WalkEnemy>(Vector2(static_cast<float>(w), static_cast<float>(h)), _pPlayer, _walkEnemyH, WalkEnemyState::Idle, false));
+				_pEnemies.push_back(std::make_shared<WalkEnemy>(pos, _pPlayer, _walkEnemyH, WalkEnemyState::Idle, false));
+				break;
+			case ObjectData::WalkEnemyMoveLeft:
+				_pEnemies.push_back(std::make_shared<WalkEnemy>(pos, _pPlayer, _walkEnemyH, WalkEnemyState::Move, true));
+				break;
+			case ObjectData::WalkEnemyMoveRight:
+				_pEnemies.push_back(std::make_shared<WalkEnemy>(pos, _pPlayer, _walkEnemyH, WalkEnemyState::Move, false));
+				break;
+			case ObjectData::FlyEnemyIdle:
+				_pEnemies.push_back(std::make_shared<FlyEnemy>(pos, _pPlayer, _flyEnemyH, FlyEnemyState::Idle));
+				break;
+			case ObjectData::FlyEnemyMove:
+				_pEnemies.push_back(std::make_shared<FlyEnemy>(pos, _pPlayer, _flyEnemyH, FlyEnemyState::Move));
+				break;
+			case ObjectData::JumpEnemy:
+				_pEnemies.push_back(std::make_shared<JumpEnemy>(pos, _pPlayer, _jumpEnemyH));
 				break;
 			}
 		}
--- WinterGame_2025/Source/Systems/EnemyManager.h
+++ WinterGame_2025/Source/Systems/EnemyManager.h
@@ -4,6 +4,8 @@
 #include "../Utility/Geometry.h"
 
 class Player;
+class Map;
+class Camera;
 class Enemy;
 
 /// <summary>
@@ -12,7 +14,7 @@ class Enemy;
 class EnemyManager
 {
 public:
-	EnemyManager(std::shared_ptr<Player> pPlayer);
+	EnemyManager(std::shared_ptr<Player> pPlayer, std::shared_ptr<Map> pMap, std::shared_ptr<Camera> pCamera);
 	~EnemyManager();
 
 	void Update();
@@ -25,11 +27,15 @@ public:
 	/// <param name="size">マップサイズ</param>
 	void Spawn(const std::vector<uint16_t>& objectData, Size size);
 
+	const std::vector<std::shared_ptr<Enemy>>& GetEnemies() const { return _pEnemies; }
+
 private:
 	int _walkEnemyH = -1;
 	int _flyEnemyH = -1;
 	int _jumpEnemyH = -1;
 
 	std::shared_ptr<Player> _pPlayer;
-	std::vector<std::shared_ptr<Enemy>> _enemies;
+	std::shared_ptr<Map> _pMap;
+	std::shared_ptr<Camera> _pCamera;
+	std::vector<std::shared_ptr<Enemy>> _pEnemies;
 };
