32597f616ad6d4e94c829d6f66e0d1de4a649029
Tiger5286 <toratiger5286@gmail.com>
2025-12-08 11:46:24 +0900

    FlyEnemy実装中
    突進攻撃〇
    規定の位置に戻る〇
    雑魚敵召喚実装中

--- WinterGame_2025/Source/GameObjects/Enemies/FlyBoss.cpp
+++ WinterGame_2025/Source/GameObjects/Enemies/FlyBoss.cpp
@@ -1,6 +1,10 @@
 #include "FlyBoss.h"
 #include "Dxlib.h"
 #include "../../Utility/CircleCollider.h"
+#include "../../Systems/Camera.h"
+#include "../../Systems/EnemyManager.h"
+#include "DroneEnemy.h"
+#include "../../Game.h"
 #include "../Player.h"
 #include <cmath>
 
@@ -18,16 +22,36 @@ namespace
 	constexpr int kColliderRadius = 70;
 
 	// 動きの制御関連
+	// 縦揺れ関連
 	constexpr float kSinAngleIncrement = 0.03;
 	constexpr int kSinAngleScale = 40;
+	// Idle状態関連
+	constexpr int kIdleWaitFrame = 60;
+	// Tackle状態関連
+	constexpr int kAimFrame = 120;
+	constexpr float kAimLerpRate = 0.05f;
+	constexpr float kRunupVel = 15.0f;		// タックル開始時の後ろに下がる時の速度
+	constexpr float kTackleAccel = 1.0f;	// タックル中の加速度
+	constexpr float kMaxTackleVel = 40.0f;	// タックル中の最大速度
+	// Back状態関連
+	constexpr int kBasePosXRight = 1600;
+	constexpr int kBasePosXLeft = 300;
+	constexpr int kBasePosY = 550;
+	constexpr float kBackSpeed = 5.0f;
+	constexpr int kBackWaitFrame = 30;
+	constexpr float kBackDisThreshold = 5.0f;
+
+	constexpr int kOutOfScreenMargin = 100;
 
 	constexpr int kHp = 100;
 	constexpr int kScore = 10000;
 }
 
-FlyBoss::FlyBoss(Vector2 pos, std::shared_ptr<Player> pPlayer, std::shared_ptr<EffectManager> pEffectManager, SceneManager& sceneManager,int handle):
+FlyBoss::FlyBoss(Vector2 pos, std::shared_ptr<Player> pPlayer,std::shared_ptr<Camera> pCamera, EnemyManager& enemyManager, std::shared_ptr<EffectManager> pEffectManager, SceneManager& sceneManager,int handle):
 	Enemy(kHp, kScore,pPlayer,pEffectManager,sceneManager),
-	_handle(handle)
+	_handle(handle),
+	_pCamera(pCamera),
+	_enemyManager(enemyManager)
 {
 	_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, kOneAnimFrame, kDrawScale);
 	_pos = ChipPosToGamePos(pos);
@@ -49,45 +73,136 @@ void FlyBoss::Update(Map& map)
 
 void FlyBoss::UpdateAnytime()
 {
-	if (_state == FlyBossState::AimTackle)
+	if (_state == FlyBossState::AimAndTackle)
 	{
-		//if ()
+		// 横位置が画面外に出たら
+		if (_pos.x < _pCamera->GetPos().x - GlobalConstants::kScreenWidth / 2 - kOutOfScreenMargin ||
+			_pos.x > _pCamera->GetPos().x + GlobalConstants::kScreenWidth / 2 + kOutOfScreenMargin)
+		{
+			_state = FlyBossState::Back;
+			_frame = 0;
+			_vel = Vector2(0, 0);
+			CheckIsPlayerOnLeft();
+		}
+	}
+
+	if (_state == FlyBossState::Back)
+	{
+		_frame++;
+		// 少し間をおいてから元の位置に戻る
+		if (_frame > kBackWaitFrame)
+		{
+			if (_isPlayerOnLeft)
+			{	// プレイヤーが左にいる
+				// 右の既定の位置に戻る
+				Vector2 targetPos = Vector2(kBasePosXRight, kBasePosY);
+				Vector2 vec = targetPos - _pos;
+				vec.Normalize();
+				_pos += vec * kBackSpeed;
+				// 既定の位置に戻ったらIdle状態へ
+				if (abs((_pos - targetPos).Length()) < kBackDisThreshold)
+				{
+					_state = FlyBossState::Idle;
+					_frame = 0;
+					_vel = Vector2(0, 0);
+				}
+			}
+			else
+			{	// プレイヤーが右にいる
+				// 左の既定の位置に戻る
+				Vector2 targetPos = Vector2(kBasePosXLeft, kBasePosY);
+				Vector2 vec = targetPos - _pos;
+				vec.Normalize();
+				_pos += vec * kBackSpeed;
+				// 既定の位置に戻ったらIdle状態へ
+				if (abs((_pos - targetPos).Length()) < kBackDisThreshold)
+				{
+					_state = FlyBossState::Idle;
+					_frame = 0;
+					_vel = Vector2(0, 0);
+				}
+			}
+		}
 	}
 }
 
 void FlyBoss::Update()
 {
-	if (_state == FlyBossState::Idle)
+	if (_state == FlyBossState::Idle)	// Idle状態
 	{
 		_frame++;
-		if (_frame > 60)
+		if (_frame > kIdleWaitFrame)
 		{
-			_state = FlyBossState::AimTackle;
-			_frame = 0;
-		}
-	}
-	else if (_state == FlyBossState::AimTackle)
-	{
-		_frame++;
-		if (_frame < 120)
-		{
-			_pos.y = std::lerp(_pos.y, _pPlayer->GetColliderPos().y, 0.05f);
-		}
-		else if (_frame == 120)
-		{
-			_isPlayerOnLeft = _pos.x - _pPlayer->GetPos().x > 0;
-			_isPlayerOnLeft ? _vel.x = 15.0f : _vel.x = -15.0f;
-		}
-		else
-		{
-			if (_isPlayerOnLeft)
+			// 敵が複数いるならタックル、そうでなければドローン召喚へ
+			if (_enemyManager.GetEnemies().size() > 1)
 			{
-				_vel.x -= 1.0f;
+				_state = FlyBossState::AimAndTackle;
 			}
 			else
 			{
-				_vel.x += 1.0f;
+				_state = FlyBossState::SummonEnemies;
 			}
+			_frame = 0;
+		}
+	}
+	else if (_state == FlyBossState::AimAndTackle)	// Tackle状態
+	{
+		_frame++;
+		if (_frame < kAimFrame)
+		{
+			_pos.y = std::lerp(_pos.y, _pPlayer->GetColliderPos().y, kAimLerpRate);
+		}
+		else if (_frame == kAimFrame)
+		{
+			CheckIsPlayerOnLeft();
+			_isPlayerOnLeft ? _vel.x = kRunupVel : _vel.x = -kRunupVel;
+		}
+		else
+		{
+			// プレイヤーのいる方向に加速
+			_isPlayerOnLeft ? _vel.x -= kTackleAccel : _vel.x += kTackleAccel;
+			// 速度制限
+			if (_vel.x > kMaxTackleVel)
+			{
+				_vel.x = kMaxTackleVel;
+			}
+			else if (_vel.x < -kMaxTackleVel)
+			{
+				_vel.x = -kMaxTackleVel;
+			}
+		}
+	}
+	else if (_state == FlyBossState::SummonEnemies)
+	{
+		// 一定間隔でドローンを召喚
+		if (_frame % 120 == 0)
+		{
+			_enemyManager.Create(ObjectData::DroneEnemy, _pos, false);
+		}
+
+		_frame++;
+
+		if (_frame == 1)
+		{	// 最初に自分がどちら側にいるかを判定しておく
+			_isOnLeft = _pos.x < _pCamera->GetPos().x;
+		}
+
+		Vector2 targetPos;
+		if (_isOnLeft)
+		{
+			targetPos = { kBasePosXRight, kBasePosY };
+		}
+		else
+		{
+			targetPos = { kBasePosXLeft, kBasePosY };
+		}
+		Vector2 vec = targetPos - _pos;
+		vec.Normalize();
+		_pos += vec * 5.0f;
+		if ((targetPos - _pos).Length() < 5.0f)
+		{
+			_state = FlyBossState::Idle;
+			_frame = 0;
 		}
 	}
 
@@ -100,6 +215,11 @@ void FlyBoss::Update()
 
 	_pCollider->SetPos(adjustPos);
 	_nowAnim.Update();
+}
+
+void FlyBoss::CheckIsPlayerOnLeft()
+{
+	_isPlayerOnLeft = _pos.x - _pPlayer->GetPos().x > 0;
 }
 
 void FlyBoss::Draw(Vector2 offset)
--- WinterGame_2025/Source/GameObjects/Enemies/FlyBoss.h
+++ WinterGame_2025/Source/GameObjects/Enemies/FlyBoss.h
@@ -1,17 +1,22 @@
 #pragma once
 #include "Enemy.h"
 
+class Camera;
+class EnemyManager;
+
 enum class FlyBossState
 {
     Idle,
-    AimTackle,
+    AimAndTackle,
+    SummonEnemies,
+    Back,
 };
 
 class FlyBoss :
     public Enemy
 {
 public:
-    FlyBoss(Vector2 pos, std::shared_ptr<Player> pPlayer, std::shared_ptr<EffectManager> pEffectManager, SceneManager& sceneManager,int handle);
+    FlyBoss(Vector2 pos, std::shared_ptr<Player> pPlayer, std::shared_ptr<Camera> pCamera, EnemyManager& enemyManager, std::shared_ptr<EffectManager> pEffectManager, SceneManager& sceneManager, int handle);
     ~FlyBoss();
 
     void Init() override;
@@ -22,12 +27,21 @@ public:
 private:
     void Update();
 
+	/// <summary>
+	/// プレイヤーが左右どちらにいるかチェックし、_isPlayerOnLeftを更新する
+	/// </summary>
+	void CheckIsPlayerOnLeft();
+
 private:
     int _handle = -1;
 
+    std::shared_ptr<Camera> _pCamera;
+    EnemyManager& _enemyManager;
+
     int _frame = 0;
     FlyBossState _state = FlyBossState::Idle;
-    bool _isPlayerOnLeft = true;
+	bool _isPlayerOnLeft = true;    // プレイヤーが左にいるかどうか
+    bool _isOnLeft = false;     // 自身が左側にいるかどうか
 
     float _sinAngle = 0.0f;
 };
