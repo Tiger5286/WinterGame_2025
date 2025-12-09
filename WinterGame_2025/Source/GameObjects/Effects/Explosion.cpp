eb8ee16563070cd21887f02237a693f1c5a31d76
Tiger5286 <toratiger5286@gmail.com>
2025-12-09 10:21:30 +0900

    Explosion定数化

--- WinterGame_2025/Source/GameObjects/Effects/Explosion.cpp
+++ WinterGame_2025/Source/GameObjects/Effects/Explosion.cpp
@@ -4,9 +4,13 @@ namespace
 {
 	constexpr int kGraphSize = 48;
 	constexpr float kDrawScale = 3.0f;
+	constexpr float kDrawScaleSmall = 1.5f;
+	constexpr float kDrawScaleBig = 6.0f;
 
 	constexpr int kAnimNum = 10;
 	constexpr int kOneAnimFrame = 4;
+	constexpr int kOneAnimFrameSmall = 2;
+	constexpr int kOneAnimFrameBig = 8;
 }
 
 Explosion::Explosion(int handle,Vector2 pos,ExplosionType type):
@@ -19,10 +23,10 @@ Explosion::Explosion(int handle,Vector2 pos,ExplosionType type):
 		_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, kOneAnimFrame, kDrawScale, false);
 		break;
 	case ExplosionType::Small:
-		_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, 2, 1.5f , false);
+		_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, kOneAnimFrameSmall, kDrawScaleSmall, false);
 		break;
 	case ExplosionType::Big:
-		_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, 8, 6.0f , false);
+		_nowAnim.Init(_handle, 0, Vector2(kGraphSize, kGraphSize), kAnimNum, kOneAnimFrameBig, kDrawScaleBig, false);
 		break;
 	default:
 		break;
