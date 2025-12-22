7608d0c5f115a2514249e97f519a92be3780faeb
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2025-12-22 14:59:03 +0900

    タイトルシーン製作中
    ステージセレクトに移行するだけだったのを、オプションとゲーム終了のボタンを追加

--- WinterGame_2025/Source/Scenes/SceneTitle.cpp
+++ WinterGame_2025/Source/Scenes/SceneTitle.cpp
@@ -12,8 +12,16 @@
 
 namespace
 {
-	constexpr int kStringFrickerInterval = 60;
+	constexpr int kTitleOffsetY = -200;
 }
+
+enum class MenuItems
+{
+	Start,
+	Option,
+	Exit,
+	Num
+};
 
 SceneTitle::SceneTitle(SceneManager& manager):
 	SceneBase(manager)
@@ -22,6 +30,14 @@ SceneTitle::SceneTitle(SceneManager& manager):
 	assert(_bgHandle != -1);
 	_titleHandle = LoadGraph("data/UI/Title.png");
 	assert(_titleHandle != -1);
+	_menuHandles[static_cast<int>(MenuItems::Start)] = LoadGraph("data/UI/StartButton.png");
+	_menuHandles[static_cast<int>(MenuItems::Option)] = LoadGraph("data/UI/OptionButton.png");
+	_menuHandles[static_cast<int>(MenuItems::Exit)] = LoadGraph("data/UI/ExitButton.png");
+	for (const auto& handle : _menuHandles)
+	{
+		assert(handle != -1);
+	}
+
 	_fontHandle = CreateFontToHandle(GlobalConstants::kMainFontName, 64, -1, DX_FONTTYPE_ANTIALIASING_EDGE_4X4);
 }
 
@@ -29,6 +45,11 @@ SceneTitle::~SceneTitle()
 {
 	DeleteGraph(_bgHandle);
 	DeleteGraph(_titleHandle);
+	for (const auto& handle : _menuHandles)
+	{
+		DeleteGraph(handle);
+	}
+
 	DeleteFontToHandle(_fontHandle);
 }
 
@@ -40,16 +61,45 @@ void SceneTitle::Update(Input& input)
 {
 	_frame++;
 
+	if (input.IsTriggered("up"))
+	{
+		_selectIndex--;
+		if (_selectIndex < 0)
+		{
+			_selectIndex = static_cast<int>(MenuItems::Num) - 1;
+		}
+	}
+	if (input.IsTriggered("down"))
+	{
+		_selectIndex++;
+		if (_selectIndex > static_cast<int>(MenuItems::Num) - 1)
+		{
+			_selectIndex = 0;
+		}
+	}
 	if (input.IsTriggered("decision"))
 	{
-		// 全てのステージが未クリアなら直接シーンメインへ
-		if (_manager.GetSaveData().clearedStage == static_cast<int>(Stages::None))
+		// メニューごとの処理
+		switch (static_cast<MenuItems>(_selectIndex))
 		{
-			_manager.ChangeSceneWithFade(std::make_shared<SceneMain>(_manager, Stages::Tutorial), FadeState::CircleFadeIn, FadeState::NormalFadeOut);
-		}
-		else	// そうでないならステージセレクトへ
-		{
-			_manager.ChangeSceneWithFade(std::make_shared<SceneStageSelect>(_manager));
+		case MenuItems::Start:	// スタート
+			// 全てのステージが未クリアなら直接シーンメインへ
+			if (_manager.GetSaveData().clearedStage == static_cast<int>(Stages::None))
+			{
+				_manager.ChangeSceneWithFade(std::make_shared<SceneMain>(_manager, Stages::Tutorial), FadeState::CircleFadeIn, FadeState::NormalFadeOut);
+			}
+			else	// そうでないならステージセレクトへ
+			{
+				_manager.ChangeSceneWithFade(std::make_shared<SceneStageSelect>(_manager));
+			}
+			return;
+		case MenuItems::Option:	// オプション
+			// 現状オプションは未実装
+			printfDx("オプションは未実装です\n");
+			break;
+		case MenuItems::Exit:	// 終了
+			// 今から実装
+			break;
 		}
 	}
 
@@ -67,21 +117,31 @@ void SceneTitle::Draw()
 	constexpr int screenW = GlobalConstants::kScreenWidth;
 	constexpr int screenH = GlobalConstants::kScreenHeight;
 
+	// 背景描画
 	DrawExtendGraph(0, 0, screenW, screenH, _bgHandle, false);
 
+	// タイトル描画
 	float sin = sinf(_frame * 0.02f);
-	DrawRotaGraph(screenW / 2, screenH / 2 + sin * 15, 1.0, 0.0, _titleHandle, true);
-
-	// "Press A to Start"を点滅させる
-	if (_frame % kStringFrickerInterval < kStringFrickerInterval / 2)
-	{
-		const std::string pressAString = "Press A to Start";
-		int strW = GetDrawStringWidthToHandle(pressAString.c_str(), static_cast<int>(pressAString.size()), _fontHandle);
-		//int strW = GetDrawStringWidth(pressAString.c_str(), static_cast<int>(pressAString.size()));
-		//DrawString(screenW / 2 - strW / 2, screenH / 2 + 250, pressAString.c_str(), 0xffffff);
-		DrawStringToHandle(screenW / 2 - strW / 2, screenH / 2 + 250, pressAString.c_str(), 0x0022bb,_fontHandle,0xffffff);
-	}
+	DrawRotaGraph(screenW / 2, (screenH / 2 + sin * 15) + kTitleOffsetY, 1.0, 0.0, _titleHandle, true);
 	
+	// メニュー描画
+	for (int i = 0; i < static_cast<int>(MenuItems::Num); i++)
+	{
+		int y = screenH / 2 + 170 + i * 130;
+		// 未選択のメニューは暗く、小さく表示
+		float scale = 0.8f;
+		SetDrawBright(128, 128, 128);
+		if (_selectIndex == i)	// 選択中のメニューは明るく、大きく表示
+		{
+			scale = 1.0f;
+			SetDrawBright(255, 255, 255);
+		}
+		DrawRotaGraph(screenW / 2, y, scale, 0.0, _menuHandles[i], true);
+		SetDrawBright(255, 255, 255);	// 明るさを元に戻す
+	}
+	//DrawRotaGraph(screenW / 2, screenH - 400, 1.0, 0.0, _startButtonH, true);
+	//DrawRotaGraph(screenW / 2, screenH - 275, 1.0, 0.0, _optionButtonH, true);
+	//DrawRotaGraph(screenW / 2, screenH - 150, 1.0, 0.0, _exitButtonH, true);
 
 #ifdef _DEBUG
 	DrawString(0, 0, "SceneTitle", 0xffffff);
--- WinterGame_2025/Source/Scenes/SceneTitle.h
+++ WinterGame_2025/Source/Scenes/SceneTitle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "SceneBase.h"
+#include <array>
 class SceneTitle : public SceneBase
 {
 public:
@@ -15,7 +16,13 @@ private:
 	int _bgHandle = -1;
 	int _titleHandle = -1;
 
+	// メニューボタンハンドル(スタート、オプション、終了)
+	std::array<int, 3> _menuHandles = { -1,-1,-1 };
+
 	// フォントハンドル
 	int _fontHandle = -1;
+
+	// 選択中のメニューインデックス
+	int _selectIndex = 0;
 };
 
