d908920825fccbe8a7bca3948ed66e3b64cefd1e
Tiger5286 <210645128+Tiger5286@users.noreply.github.com>
2026-01-07 20:03:39 +0900

    デバッグシーンから隠しステージに直接飛べるように

--- WinterGame_2025/Source/Scenes/DebugScene.cpp
+++ WinterGame_2025/Source/Scenes/DebugScene.cpp
@@ -21,7 +21,7 @@ DebugScene::DebugScene(SceneManager& manager):
 		"SceneStageSelect"
 	};
 	_execTable["SceneMain"] = [this]() {
-		_manager.ChangeScene(std::make_shared<SceneMain>(_manager,Stages::Boss1),FadeState::CircleFadeIn);
+		_manager.ChangeScene(std::make_shared<SceneMain>(_manager,Stages::SecretStage),FadeState::CircleFadeIn);
 		};
 	_execTable["SceneTitle"] = [this]() {
 		_manager.ChangeScene(std::make_shared<SceneTitle>(_manager));
