a1de1debb8d27116d57f53b978cbf773897f8868
Tiger5286 <toratiger5286@gmail.com>
2026-01-14 10:56:00 +0900

    TutorialUIがボタン画像を持つように

--- WinterGame_2025/Source/UI/TutorialUI.cpp
+++ WinterGame_2025/Source/UI/TutorialUI.cpp
@@ -1,10 +1,60 @@
 #include "TutorialUI.h"
 #include "Dxlib.h"
 #include "../GameObjects/Player.h"
+#include <string>
+#include <cassert>
+
+namespace
+{
+	// 画像リスト
+	enum class TutorialGraphs
+	{
+		ButtonA,
+		ButtonAOutline,
+		ButtonB,
+		ButtonBOutline,
+		ButtonX,
+		ButtonXOutline,
+		DPad,
+		DPadLeft,
+		DPadRight,
+
+		Num
+	};
+	// ロードする画像のファイル名
+	const std::string kGraphFileNames[] =
+	{
+		"data/UI/Buttons/button_a.png",
+		"data/UI/Buttons/button_a_outline.png",
+		"data/UI/Buttons/button_b.png",
+		"data/UI/Buttons/button_b_outline.png",
+		"data/UI/Buttons/button_x.png",
+		"data/UI/Buttons/button_x_outline.png",
+		"data/UI/Buttons/dpad.png",
+		"data/UI/Buttons/dpad_left.png",
+		"data/UI/Buttons/dpad_right.png"
+	};
+	// 画像の枚数とenumの数が一致しているか確認
+	constexpr int size = sizeof(kGraphFileNames) / sizeof(kGraphFileNames[0]);
+	static_assert(static_cast<int>(TutorialGraphs::Num) == size);
+}
 
 TutorialUI::TutorialUI(Player& player):
 	_player(player)
 {
+	for (int i = 0; i < static_cast<int>(TutorialGraphs::Num); i++)
+	{
+		_graphHandles.push_back(LoadGraph(kGraphFileNames[i].c_str()));
+		assert(_graphHandles.back() != -1 && "チュートリアル画像を正しくロードできませんでした");
+	}
+}
+
+TutorialUI::~TutorialUI()
+{
+	for (const auto& handle : _graphHandles)
+	{
+		DeleteGraph(handle);
+	}
 }
 
 void TutorialUI::Update(Input& input)
--- WinterGame_2025/Source/UI/TutorialUI.h
+++ WinterGame_2025/Source/UI/TutorialUI.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../Utility/Geometry.h"
+#include <vector>
 
 class Input;
 class Player;
@@ -8,11 +9,14 @@ class TutorialUI
 {
 public:
 	TutorialUI(Player& player);
+	~TutorialUI();
 
 	void Update(Input& input);
 	void Draw(const Vector2& offset);
 
 private:
 	Player& _player;
+
+	std::vector<int> _graphHandles;
 };
 
