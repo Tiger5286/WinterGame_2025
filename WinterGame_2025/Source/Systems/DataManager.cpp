90fbd52aa6777d128c90fc9f8833150bba93e707
Tiger5286 <toratiger5286@gmail.com>
2026-01-14 12:26:07 +0900

    DataManagerコメント追加

--- WinterGame_2025/Source/Systems/DataManager.cpp
+++ WinterGame_2025/Source/Systems/DataManager.cpp
@@ -18,10 +18,12 @@ DataManager::~DataManager()
 
 void DataManager::Save()
 {
+	// ファイルを開く
 	FILE* fp = nullptr;
 	auto err = fopen_s(&fp, kDataFileName, "wb");
 	if (fp == nullptr) return;
 
+	// 書き込み用のヘッダを用意
 	SaveDataHeader header;
 	header.signature[0] = 's';
 	header.signature[1] = 'd';
@@ -30,18 +32,28 @@ void DataManager::Save()
 	header.version = 1.0f;
 	header.size = sizeof(_saveData);
 
+	// ヘッダの書き込み
 	fwrite(&header, sizeof(header), 1, fp);
+	// セーブデータ本体の書き込み
 	fwrite(&_saveData, sizeof(_saveData), 1, fp);
+	// ファイルを閉じる
 	fclose(fp);
 }
 
 void DataManager::Load()
 {
+	// ファイルを開く
 	auto fileName = kDataFileName;
 	int handle = FileRead_open(fileName);
+
+	// 受け取り用のヘッダを用意
 	SaveDataHeader header;
+
+	// ヘッダを読み込み
 	FileRead_read(&header, sizeof(header), handle);
+	// セーブデータ本体を読み込み
 	FileRead_read(&_saveData, sizeof(_saveData), handle);
 
+	// ファイルを閉じる
 	FileRead_close(handle);
 }
